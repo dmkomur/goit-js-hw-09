b302d65ee1397a066c27e97cb473cd5d2723032d
dmkomur <125804681+dmkomur@users.noreply.github.com>
2023-04-25 20:23:37 +0300

    3done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,35 +1,44 @@
+const formRef = document.querySelector('.form');
+
+const refs = {
+   delay: formRef.elements.delay,
+   step: formRef.elements.step,
+   amount: formRef.elements.amount,    
+}
+
+formRef.addEventListener('submit', onFormSubmit);
 
 
 
+function onFormSubmit(event) {
+  event.preventDefault();
+  const delay = Number(refs.delay.value);
+  let totalDelay = delay;
 
-
-
-
-
-
-
-
-
-
-
-
-
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+    for (let i = 1; i <= Number(refs.amount.value); i += 1) {
+      if (i > 1) ( totalDelay = delay + Number(refs.step.value))
+  createPromise(i, totalDelay).then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+};
 }
 
 
+function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random();
 
-
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    setTimeout (() => {
+    if (shouldResolve  >  0.5) {
+    resolve({position, delay});
+    }
+    else {
+    reject({position, delay});
+    }
+    }, delay)
   });
+  return promise;
+}
